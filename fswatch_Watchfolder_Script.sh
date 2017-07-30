536c93cb5000b22ac4180bbae63b2d20bfe371ec
digitaltvguy <la@loudimage.com>
2017-07-29 20:52:04 -0400

    Added log files; changed scp to rsync to gain transfer logs and more stats

--- fswatch_Watchfolder_Script.sh
+++ fswatch_Watchfolder_Script.sh
@@ -1,18 +1,45 @@
 #! /bin/bash
 
+
+# ********************************************************
+# CONFIGURE START
+# ********************************************************
+
+# ********************************************************
+#           REMINDERS
+# 1. Add Arcfour256 to sshd_config file
+# 2. Add public key from your source machine to destination server to avoid authentication request for scp copy
+# 3. Add config file to /etc/newssyslog.d folder (fswatch_watchfolder.conf) to manage logs
+#
+# ********************************************************
+
+
+
 # identify location of fswatch binary
 FSWATCH_PATH="/usr/local/bin/fswatch"
+sshPath="/usr/bin/ssh"
+
+# Redirect Log location ***REMEMBER TO CREATE the file fswatch_watchfolder.log and set permissions to 777***
+LOG_FILE_PATH="/Library/Logs/fswatch_watchfolder.log"    
+exec >> "$LOG_FILE_PATH"
+exec 2>&1
+
 
 # identify local location of Watchfolder
-LOCAL_WATCHFOLDER_PATH="<Local WatchFolder Path>"
+LOCAL_WATCHFOLDER_PATH="<Path to WatchFolder>"
 
 # Set Finished File Folder Path
-TARGET_TRANSITION_PATH="<Local Transition Destination Path>"
+TARGET_TRANSITION_PATH="<Path to Transition Folder>"
 # Set remote path if using remote destination
-REMOTE_DESTINATION_PATH="user@<IP>:<Remote File Path>"
+REMOTE_DESTINATION_PATH="user@<IP>:<REMOTE PATH>"
 
 # fswatch parameter to control how often a trigger will be detected when watching for a folder change
 LATENCY=3
+
+# ********************************************************
+# CONFIGURE END
+# ********************************************************
+
 
 # Watch for changes and sync (exclude hidden files)
 echo    "Watching for changes. Quit anytime with Ctrl-C."
@@ -48,14 +75,22 @@ sleep 1
 if [ -f "$TriggerFilePath" ]
 	then
 echo ""
-echo "$TriggerFilePath File exists"
+echo "$TriggerFilePath File exists in watchfolder"
+echo "Moving file to Transition Folder"
 mv "$TriggerFilePath" "$TARGET_TRANSITION_PATH"
-echo "$TriggerFileBaseName"
+#echo "$TriggerFileBaseName"
 scpFilePathSource="${TARGET_TRANSITION_PATH}${TriggerFileBaseName}"
 
 # Copy file(s) using faster arcfour256 cipher - This for this cipher must be added to sshd config file
 # A public ssh key needs to be set up to avoid authentication requests from scp
-scp -o Cipher=arcfour256 "$scpFilePathSource" "$REMOTE_DESTINATION_PATH"
+echo "Copying "$TriggerFileBaseName" to Destination Server"
+	rsync -aW \
+	--size-only \
+    --include-from="$TempFileName" \
+    --stats \
+    -e "ssh -T -c arcfour256 -o Compression=no -x" \
+    "$scpFilePathSource" "$REMOTE_DESTINATION_PATH" \
+    | tee -a "$LOG_FILE_PATH"
 	else
 echo ""
 #echo "$TriggerFilePath File has been removed from watchfolder"
